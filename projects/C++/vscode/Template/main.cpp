0c858137299c17ea9ebc50530545b857dcedf007
Maria marcano <mariangemarcano@gmail.com>
2019-10-14 17:15:55 +0200

    Fix formatting

--- projects/C++/vscode/Template/main.cpp
+++ projects/C++/vscode/Template/main.cpp
@@ -7,9 +7,9 @@ using namespace std;
 int main()
 {
 
-    vector<string> msg{"Hello", "C++", "World", "from", "VS Code!"};
+    vector<string> msg {"Hello", "C++", "World", "from", "VS Code!"};
 
-    for (const string &word : msg)
+    for (const string& word : msg)
     {
         cout << word << " ";
     }
